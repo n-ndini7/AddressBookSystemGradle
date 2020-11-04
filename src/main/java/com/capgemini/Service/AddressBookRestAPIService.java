ffe24beec55913c18decf91deb50bb50f9d93497
n-ndini7 <hp@RJs-PC>
2020-11-04 19:14:53 +0530

    [Nandini] Added UC21 - add contacts to the json server

---
+++ src/main/java/com/capgemini/Service/AddressBookRestAPIService.java
@@ -0,0 +1,23 @@
+package com.capgemini.Service;
+
+import java.util.List;
+
+import com.capgemini.ContactInfo;
+
+public class AddressBookRestAPIService {
+
+	public List<ContactInfo> contactList = null;
+
+	public AddressBookRestAPIService(List<ContactInfo> contacts) {
+		contactList = contacts;
+	}
+
+	public long countEntries() {
+		return contactList.size();
+	}
+
+	public void addContactToJsonServer(ContactInfo contact1) {
+		contactList.add(contact1);
+	}
+
+}
